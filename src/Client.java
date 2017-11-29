34d56b2b1b99eb9b2bbae252408567217955ed64
fy2012 <fy2012@gmail.com>
2017-11-28 22:04:22 -0800

    UPPERCASE worked

--- src/Client.java
+++ src/Client.java
@@ -2,20 +2,56 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+
 public class Client
 {
+    private BufferedReader in;
+    private PrintWriter out;
+
     public static void main(String[] args) throws Exception
     {
-        Scanner kb = new Scanner(System.in);
-        //System.out.print(">");
-        // String command = kb.next();
         Client CLT = new Client();
-        CLT.run();
+        CLT.connect();
     }
 
+    public void connect() throws Exception
+    {
+        Socket SK = new Socket("localhost",1025);
+        in = new BufferedReader(new InputStreamReader(SK.getInputStream()));
+        out = new PrintWriter(SK.getOutputStream(), true);
+        System.out.println(in.readLine());
+
+        Scanner kb = new Scanner(System.in);
+        System.out.print("c: ");
+        String input = kb.nextLine();
+        int count;
+
+        while(!input.equals("EXIT"))
+        {
+            out.println(input);
+            System.out.println(in.readLine());
+            count = 0;
+            while(!input.equals("."))
+            {
+                count++;
+                System.out.print("c: ");
+                input = kb.nextLine();
+                out.println(input);
+            }
+
+            for(int i = 0; i < count-1; i++)
+            {
+                System.out.println(in.readLine());
+            }
+
+            System.out.print("c: ");
+            input = kb.nextLine();
+        }
+    }
+    /*
     public void run() throws Exception
     {
-        Socket sk = new Socket("localhost",1028);
+        Socket sk = new Socket("localhost",1025);
 
         PrintStream PS = new PrintStream(sk.getOutputStream());
         String command = "connect";
@@ -26,5 +62,5 @@ public class Client
 
         String MESSAGE = BR.readLine();
         System.out.println(MESSAGE);
-    }
+    }*/
 }
--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,91 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Server
 {
     public static void main(String[] args) throws Exception
     {
-        Server SER = new Server();
-        SER.run();
+        ServerSocket srvSk = new ServerSocket(1025);
+        try
+        {
+            while(true)
+            {
+                new trd(srvSk.accept()).start();
+            }
+        }
+        finally
+        {
+            srvSk.close();
+        }
     }
 
+    private static class trd extends Thread
+    {
+        private Socket SK;
+
+        public trd(Socket SK)
+        {
+            this.SK = SK;
+        }
+
+        public void run()
+        {
+            try
+            {
+                BufferedReader in = new BufferedReader(new InputStreamReader(SK.getInputStream()));
+                PrintWriter out = new PrintWriter(SK.getOutputStream(), true);
+                ArrayList<String> list=new ArrayList<String>();
+                out.println("server: got connection from client " + SK.getLocalAddress().getHostAddress());
+
+                while(true)
+                {
+                    String input = in.readLine();
+                    if (input.equals("UPPERCASE"))
+                    {
+                        out.println("s: 200 OK");
+                        input = in.readLine();
+                        while(!input.equals("."))
+                        {
+                            list.add(input.toUpperCase());
+                            input = in.readLine();
+                        }
+                        Iterator itr=list.iterator();
+                        while(itr.hasNext())
+                        {
+                            out.println("s: " + itr.next());
+                        }
+                    }
+
+                    if (input.equals("LOWERCASE"))
+                    {
+                        out.println("200 OK");
+                    }
+
+                    if (input.equals("REVERSE"))
+                    {
+                        out.println("200 OK");
+                    }
+
+                    if (input.equals("EXIT"))
+                    {
+                        out.println("200 OK");
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                System.out.println("");
+            }
+        }
+
+    }
+
+    /*
     public void run() throws Exception
     {
-        ServerSocket srvSk = new ServerSocket(1028);
+
         try
         {
             while(true)
@@ -34,5 +108,5 @@ public class Server
         {
             srvSk.close();
         }
-    }
+    }*/
 }
